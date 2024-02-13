64f0229b7d49c863544c6e63bc788f4ddfd81d92
Ilya Lopatin <75094158+lekabot@users.noreply.github.com>
2024-02-13 16:00:00 +0700

    Delete matrix+ directory

--- matrix+/tests/s21_matrix_oop_tests.c
+++
@@ -1,5 +0,0 @@
-// #include "s21_matrix_oop_tests.h"
-
-int main(void) {
-
-}
--- matrix+/tests/s21_matrix_oop_tests.h
+++
@@ -1,9 +0,0 @@
-#ifndef __S21_MATRIX_OOP_TESTS_H__
-#define __S21_MATRIX_OOP_TESTS_H__
-
-#include <gtest/gtest.h>
-#include "../s21_matrix_oop.h"
-
-int main();
-
-#endif
